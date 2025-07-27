2e2982d5ff5fd095072dc06285ba23fa1ad730bc
Raime42 <jacksonjavier100@gmail.com>
2025-07-27 19:14:23 -0400

    Login page con react y css
    El anterior login era con html y css y javascript, y para diferenciarlo del index solo se utilizó React con css para mostrar que se puede realizar un diseño que cumpla con poco.

---
+++ login-react/Login-DrJoseGregorio/componentes/login/login.jsx
@@ -0,0 +1,45 @@
+    import "./login.css";
+    import { useState } from "react";
+
+    const Login = () =>{
+
+        const [userName, setUserName] = useState("");
+        const [password, setPassword] = useState("");
+        const [dataSend, setDataSend] = useState(false);
+
+        const inputHandlers = {
+            mail: setUserName,
+            password:setPassword
+        }
+
+        const handleInputChange = (event) =>{
+            const {name, value} = event.target;
+            const handler = inputHandlers[name];
+        if (handler){
+            handler(value);
+        }
+        }
+
+        const handleDataSend = (event) => {
+            event.preventDefault();
+            console.log(userName);
+            console.log(password);
+            setDataSend(true);
+        }
+
+        const errorMail = ((userName === "" || !userName.includes("@")) && dataSend);
+        const errorPass = ((password === "" || password.length < 6) && dataSend);
+        return (
+            <div className="login-form-container">
+            <form className="login-form">
+                <h1>Inicia sesión</h1>
+                <input type="text" name="mail" placeholder="Usuario" className={`input-field ${errorMail ? "input-field-error" : ""}`} onChange={(event) => handleInputChange(event)} />
+                <input type="password" name="password" placeholder="Contraseña" className={`input-field ${errorPass ? "input-field-error" : ""}`} onChange={(event) => handleInputChange(event)}/>
+                <button type="submit" className="submit-button" onClick={(event)=>handleDataSend(event)}>Login</button>
+            </form>
+        </div>
+        );
+        
+    }
+
+    export default Login;
