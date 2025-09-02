76ae8c96df1e869e9758438d917ec53f0952ee47
Nelpena <nelpe611@gmail.com>
2025-09-02 01:13:43 -0400

    Revert "Add files via upload"
    This reverts commit 5423b22f665a43edd9fb68fb49a7f5fdb228a4c0.

--- Jose Gregorio/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const carousel = document.querySelector('#joseCarousel');
-    carousel.addEventListener('mouseenter', () => {
-        new bootstrap.Carousel(carousel).pause();
-    });
-    carousel.addEventListener('mouseleave', () => {
-        new bootstrap.Carousel(carousel).cycle();
-    });
-});
